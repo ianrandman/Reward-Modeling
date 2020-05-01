0f75b535bcd93c9106b3aa83ad04aa04403d04b2
Ian Randman <ixr5487@g.rit.edu>
2020-05-01 02:47:52 -0400

    Basic Atari agent

---
+++ main.py
@@ -0,0 +1,56 @@
+import gym
+import argparse
+
+
+class Agent(object):
+    """The world's simplest agent!"""
+    def __init__(self, action_space):
+        self.action_space = action_space
+
+    # You should modify this function
+    def act(self, observation, reward, done):
+        return self.action_space.sample()
+
+
+def main():
+    parser = argparse.ArgumentParser(description=None)
+    parser.add_argument('--env_id', nargs='?', default='Berzerk-v0', help='Select the environment to run')
+    args = parser.parse_args()
+
+    print(args.env_id)
+    env = gym.make(args.env_id)
+
+    # You provide the directory to write to (can be an existing
+    # directory, including one with existing data -- all monitor files
+    # will be namespaced). You can also dump to a tempdir if you'd
+    # like: tempfile.mkdtemp().
+    outdir = 'random-agent-results'
+
+    env.seed(0)
+    agent = Agent(env.action_space)
+
+    episode_count = 100
+    reward = 0
+    done = False
+    score = 0
+    special_data = {}
+    special_data['ale.lives'] = 3
+    ob = env.reset()
+    while not done:
+        action = agent.act(ob, reward, done)
+        ob, reward, done, x = env.step(action)
+        score += reward
+        # env.render()
+
+    agent.act(ob, reward, done)
+
+    # agent_num_levels = agent.num_levels
+    # agent_total_steps = agent.total_steps
+    # agent_elapsed_time = agent.elapsed_time
+    # Close the env and write monitor result info to disk
+    print ("Your score: %d" % score)
+    env.close()
+
+
+if __name__ == '__main__':
+    main()
