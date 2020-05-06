63c1640754e0945e8001f65153024dbdf92b2425
David <dad9489@rit.edu>
2020-05-06 02:03:34 -0400

    update README

--- src/main.py
+++ src/main.py
@@ -14,9 +14,8 @@ def main():
     threading.Thread(target=app.run, args=['0.0.0.0', 5000]).start()
 
     for env in env_lst:
-        if env == env_lst[0]:
-            training_system = TrainingSystem(env, record=True, use_reward_model=False, load_model=False)
-            multiprocessing.Process(target=training_system.play).start()
+        training_system = TrainingSystem(env, record=True, use_reward_model=False, load_model=False)
+        multiprocessing.Process(target=training_system.play).start()
 
 
 if __name__ == "__main__":
